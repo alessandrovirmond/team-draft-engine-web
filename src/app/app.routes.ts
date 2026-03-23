0de9af4fa6853be037e9bdeab01dc61142ee7e92
Alessandro Virmond <avirmond2000@gmail.com>
2026-03-23 17:58:47 -0300

    fix: disable ssr in angular.json to resolve document not defined error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PlayerListComponent } from './pages/player-list/player-list.component';
+import { DraftComponent } from './pages/draft/draft.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/players', pathMatch: 'full' },
+  { path: 'players', component: PlayerListComponent },
+  { path: 'draft', component: DraftComponent },
+  { path: '**', redirectTo: '/players' }
+];
